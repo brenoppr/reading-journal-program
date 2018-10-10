481eb3145d1ff2833168465cf2a497a4492a51f8
brenoppr <41521785+brenoppr@users.noreply.github.com>
2018-10-10 19:47:17 -0300

    Add files via upload

---
+++ prog.bat
@@ -0,0 +1,57 @@
+@ECHO OFF
+
+COLOR 70
+ECHO Type what chapter you want to read my summary of. Type it in the form of "chapter x" (without quotation marks) with the number of the chapter you want to read about in the place of x.
+:Question
+
+SET /P chapter=Enter what you want to read:
+
+IF "%chapter%"=="chapter 1" GOTO 1
+
+IF "%chapter%"=="chapter 2" GOTO 2
+
+IF "%chapter%"=="chapter 3" GOTO 3
+IF "%chapter%"=="3" GOTO 3
+IF "%chapter%"=="hapter 3" GOTO 3
+IF "%chapter%"=="chaptr 3" GOTO 3
+IF "%chapter%"=="chapter3" GOTO 3
+IF "%chapter%"=="chapter 4" GOTO 4
+IF "%chapter%"=="4" GOTO 4
+IF "%chapter%"=="hapter 4" GOTO 4
+IF "%chapter%"=="chaptr 4" GOTO 4
+IF "%chapter%"=="chapter4" GOTO 4
+IF "%chapter%"=="chapter 11" GOTO 11
+ECHO Invalid Chapter, press any key to go back to the program
+PAUSE >nul
+
+GOTO Question
+:1
+
+ECHO Chapter 1: a "normal" day
+ECHO Shane introduces himself and what a "normal day" in his family looks like. He lives with his parents, his brother and his two cats. It sounds pretty much what my family is like, even though he says his family's idea of normalcy should be really different than others' because of his disability.
+
+ECHO Sure, there are some parts that wouldn't happen in the family of a non-disabled person, like his brother having to help him get out of bed for example, but nothing really unexpected for someone in his physical conditions is apparent in this first chapter.
+GOTO Question
+:2
+ECHO Chapter 2: the lazy baby
+ECHO Shane talks about his disease and his family`s reaction when they found out about his SMA. His parents were devastated with the idea of their children never being able to walk, but they came to accept it naturally. This is a really strong part of the book, and it shows Shane's family incredible willpower. It also is the chapter that features the most parts with technical information about Shane's disease.
+GOTO Question
+:3
+ECHO Chapter 3: learning to drive
+ECHO Shane tells us more about his different weelchairs and the problems he had with them. His chairs are really expensive, 28000 dollars according to him, which makes me think if he had been born in a poor family in Brazil he wouldn't have had the same privilege be able to afford such an expensive equipment.
+GOTO Question
+:4
+ECHO Chapter 4: whoops
+ECHO Shane talks about a regular summer day for him, playing with his friend Ben. He takes this opportunity to also talk about his struggles with going to the bathroom, and how he rather wait until he feels what he calls the "full bladder pain" to go to the bathroom. However, this often went wrong, as in the case of this chapter where he ends up peeing his pants.
+ECHO This makes us think less about the macroscopic aspect of having a disability and more about how it might affects things you do daily and often go overlooked: in this case, going to the bathroom. 
+
+GOTO Question
+:11
+ECHO Chapter 11: slam dunk
+ECHO Shane tell us about an episode from when he was in third grade. He, his brother and their friend Pat were playing basketball when of them threw around the idea of trying to dunk. They tried multiple setups for doing it, and the one they settled with consisted of, in his words, "tying a noose on one end of the rope for Shane's brother to lie in, looping the rope up over the basketball hoop and attaching the other end to his wheelchair".
+
+ECHO Needless to say, it didn't work, but it shows how Shane is always trying to help out his friends, and just be normal even though his disability really does affect his capabilities of playing with his friends. 
+GOTO Question
+:15
+ECHO Chapter 15: cripple camp
+ECHO In this chapter Shane talks about when he went to a camp for kids with muscular distrophy, and his problems with an attention grabbing kid called Tim when he was there. This chapter is one that really shows a lot about Shane's personality, and his social attitudes. Instead of just playing around with the kids at the camp, Shane rather spent time with the counselors who were closer to his level of maturity. 
